907bf8f6646da5dbac2221ac5d84b3aa96debb93
maxVoormolen <33901303+maxVoormolen@users.noreply.github.com>
2019-10-01 11:45:59 +0200

    Add files via upload

---
+++ Assets/Explode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject TNT;
+    public GameObject ParticleSystem0;
+    public GameObject ParticleSystem1;
+    public GameObject ParticleSystem2;
+    public GameObject ParticleSystem3;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+            TNT.SetActive(false);
+            ParticleSystem0.SetActive(true);
+            ParticleSystem1.SetActive(true);
+            ParticleSystem2.SetActive(true);
+            ParticleSystem3.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            TNT.SetActive(true);
+            ParticleSystem0.SetActive(false);
+            ParticleSystem1.SetActive(false);
+            ParticleSystem2.SetActive(false);
+            ParticleSystem3.SetActive(false);
+        }
+    }
+}
---
+++ Assets/UpForse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpForse : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Rigidbody.velocity = transform.up * 10;
+    }
+}
